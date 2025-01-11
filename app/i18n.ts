a5484d02b52d64e0cabff61fa857d6543c7f200b
yuemori <moonoverwalker@gmail.com>
2025-01-12 01:33:28 +0900

    Add translation-check

--- app/i18n.ts
+++ app/i18n.ts
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+import { i18nextPlugin } from "translation-check";
 
 import ja from "./locales/ja.json";
 import en from "./locales/en.json";
@@ -18,6 +19,7 @@ const resources = {
 i18n
   .use(Backend)
   .use(LanguageDetector)
+  .use(i18nextPlugin)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
