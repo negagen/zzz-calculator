934f86801b04c88923b0d0cad6afdc8b7be83831
yuemori <moonoverwalker@gmail.com>
2025-01-12 02:18:58 +0900

    Fix to use i18nextPlugin is dev only

--- app/i18n.ts
+++ app/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import { i18nextPlugin } from "translation-check";
+// import { i18nextPlugin } from "translation-check";
 
 import ja from "./locales/ja.json";
 import en from "./locales/en.json";
@@ -19,7 +19,7 @@ const resources = {
 i18n
   .use(Backend)
   .use(LanguageDetector)
-  .use(i18nextPlugin)
+  // .use(i18nextPlugin)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
